ad5f290b62194776836f5b99e0f020f007c5d1b5
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-01-16 22:01:50 -0500

    added more advanced JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,31 @@
-// var topics = "HTML, CSS, Git, JavaScript";
-// console.log(topics);
-
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+}
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's Study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else {
+        console.log("Please try again!");
+    }
 }
 
-// if (topic === 'HTML') {
-//     console.log("Let's Study HTML!");
-// } else if (topic === 'CSS') {
-//     console.log("Let's study CSS!");
-// } else if (topic === 'JavaScript') {
-//     console.log("Let's study JavaScript!");
-// } else if (topic === 'Git') {
-//     console.log("Let's study Git!");
-// } else {
-//     console.log("Please try again!");
-// }
+function helloWorld() {
+    console.log("Hello, world, I am a function!");
+}
+
+console.log("Here are the topics we learned through Prework: ");
+listTopics();
+
+console.log("Which topic should we study first?");
+selectTopic();
